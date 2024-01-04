a3c157969d48ace3ca5b607808bb16c284707758
fatihyanik <fatihyanik07@gmail.com>
2024-01-04 14:02:55 +0100

    added link

---
+++ nextjs_course/app/portfolio/page.js
@@ -0,0 +1,7 @@
+const Portfolio = () => {
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+export default Portfolio
--- nextjs_course/app/users/page.js
+++
@@ -1,7 +0,0 @@
-const Users = () => {
-  return (
-    <div>This is a Users page</div>
-  )
-}
-
-export default Users
