00ba5b1df5742864d4a0893aedaa8b295d2fb6be
fatihyanik <fatihyanik07@gmail.com>
2024-01-04 16:09:42 +0100

    added useRouter()

--- nextjs_course/app/about/page.js
+++ nextjs_course/app/about/page.js
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div>This is About Page</div>
   )
 }
 
--- nextjs_course/app/page.js
+++ nextjs_course/app/page.js
@@ -1,13 +1,20 @@
 'use client'
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const Home = () => {
   const router = useRouter();
 
+  const navigate= (page)=>{
+    router.push(page);
+  }
+
   return (
     <section>
       <h1>useRouter</h1>
-      <button className="border px-2 py-4" onClick={()=>router.push("about")}>Go To About Page</button>
+      <Link className="border px-2 py-4" href='/projects'>Projects</Link>
+      <button className="border px-2 py-4" onClick={()=>navigate('about')}>Go To About Page</button>
+      <button className="border px-2 py-4" onClick={()=>navigate('login')}>Go To Login Page</button>
 
     </section>
   )
