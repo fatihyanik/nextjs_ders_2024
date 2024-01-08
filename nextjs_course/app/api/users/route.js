7936ccf7c55aee766a62090ebd2fda5c03160446
fatihyanik <fatihyanik07@gmail.com>
2024-01-08 20:01:26 +0100

    added postman

--- nextjs_course/app/api/users/route.js
+++ nextjs_course/app/api/users/route.js
@@ -1,7 +1,18 @@
 import { NextResponse } from "next/server";
-import { users } from "@/app/util/db";
 
-export function GET() {
-    const data = users
-    return NextResponse.json({data})
+export async function POST(req, res) {
+    // let data = await req.json();
+    // console.log(data);
+
+     let {email, password} = await req.json();
+    // console.log(email, password);
+
+    if (!email || !password) {
+        return NextResponse.json(
+            { error: "resquired filed not found" },
+            { status: 400 }
+        )
+    }
+
+    return NextResponse.json({ res: "Credentials verified, User succesfully logged in." });
 }
