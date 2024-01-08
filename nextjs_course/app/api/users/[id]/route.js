285221b9dd00655ea30f66bf67c8b5ac68ce4401
fatihyanik <fatihyanik07@gmail.com>
2024-01-08 18:35:56 +0100

    added get method in server component

---
+++ nextjs_course/app/api/users/[id]/route.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export function GET(_, response){
+    console.log(response.params.id);
+
+    const {id} = response.params;
+
+    return NextResponse.json({id});
+}
---
+++ nextjs_course/app/util/db.js
@@ -0,0 +1,51 @@
+export const users = [
+    {
+        id: "1",
+        name: "Alex Merson",
+        age: 29,
+        email: "alexmerson@gmail.com",
+        password: "alexmerson12",
+    },
+    {
+        id: "1",
+        name: "Alex Merson",
+        age: 29,
+        email: "alexmerson@gmail.com",
+        password: "alexmerson12",
+    },
+    {
+        id: "2",
+        name: "Fatih",
+        age: 30,
+        email: "fatih@gmail.com",
+        password: "fatih",
+    },
+    {
+        id: "3",
+        name: "Erol",
+        age: 20,
+        email: "erol@gmail.com",
+        password: "erol12",
+    },
+    {
+        id: "4",
+        name: "Saliha",
+        age: 18,
+        email: "saliha@gmail.com",
+        password: "saliha12",
+    },
+    {
+        id: "5",
+        name: "Mehmet Karaca",
+        age: 33,
+        email: "mehmetkaraca@gmail.com",
+        password: "mehmet12",
+    },
+    {
+        id: "6",
+        name: "Bahadir Inanc",
+        age: 39,
+        email: "bahadir@gmail.com",
+        password: "bahadir12",
+    },
+]
