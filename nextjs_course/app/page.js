981153ada17ba0e53f3983463f32c7c3a1c875bb
fatihyanik <fatihyanik07@gmail.com>
2024-01-08 13:54:28 +0100

    added fetching data client component

--- nextjs_course/app/page.js
+++ nextjs_course/app/page.js
@@ -1,13 +1,7 @@
-'use client'
-import { Roboto } from "next/font/google";
-
-const roboto = Roboto({subsets: ['latin'], weight:'500'})
-
 const Home = () => {
   return (
     <section>
-      <h1>Google Fonts</h1>
-      <h2 className={roboto.className}>Hello this is roboto font</h2>
+      <h1>NextJs Course</h1>
     </section>
   )
 }
