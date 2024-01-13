c4794d2e49f70d247cc32b22733bfca879b200d2
fatihyanik <fatihyanik07@gmail.com>
2024-01-13 13:40:00 +0100

    connected mongodb

---
+++ next_book_app/models/Contact.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ContactSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+}, { timestamps: true })
+
+export default mongoose?.models?.Contact || mongoose.model("Contact", ContactSchema)
